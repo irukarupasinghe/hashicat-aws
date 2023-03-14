15cfce0649788f6db07e085143da7f0794f39a68
irukarupasinghe <iruka.rupasinghe@servian.com>
2023-03-14 00:49:10 +0000

    Added the second tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "servian-cog"
     workspaces {
       name = "hashicat-aws"
